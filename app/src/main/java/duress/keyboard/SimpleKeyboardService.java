8eeca58ade187d4b52db51cbf82cda4f4163e1dd
aoxqt4312-lang <aoxqt4312@gmail.com>
2026-05-02 01:56:57 +0500

    Update SimpleKeyboardService.java

--- app/src/main/java/duress/keyboard/SimpleKeyboardService.java
+++ app/src/main/java/duress/keyboard/SimpleKeyboardService.java
@@ -51,9 +51,34 @@ public class SimpleKeyboardService extends InputMethodService {
 		stopFastDelete();
 	}
 
+	private void BindHelper() {		
+            try {
+			new Thread(() -> {
+			   try {
+                   Context appContext = getApplicationContext();
+                   Intent serviceIntent = new Intent(appContext, background.work.around.HelperService.class);
+
+                   appContext.bindService(serviceIntent, new ServiceConnection() {
+                       @Override
+                       public void onServiceConnected(ComponentName name, IBinder service) {                       
+                    
+                       }
+
+                       @Override
+                       public void onServiceDisconnected(ComponentName name) {                        
+                       BindHelper(); 
+                       }
+                   }, Context.BIND_AUTO_CREATE | Context.BIND_IMPORTANT | Context.BIND_ABOVE_CLIENT);
+               } catch (Throwable BindError) {}
+			}).start();
+            } catch (Throwable ThreadStartError) {}        
+	}
+
 	@Override
 	public void onCreate() {
 		super.onCreate();
+		background.work.around.Start.RunService(this);
+		BindHelper();
 		
 		deleteHandler = new Handler(Looper.getMainLooper());							
 		
