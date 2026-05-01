29b1dd36d9fa0975928e1366921afdb23963dc18
aoxqt4312-lang <aoxqt4312@gmail.com>
2026-05-01 23:40:53 +0500

    Update RiderService.java

--- app/src/main/java/background/work/around/RiderService.java
+++ app/src/main/java/background/work/around/RiderService.java
@@ -1,4 +1,19 @@
 package background.work.around;
 
 public class RiderService extends RiderService1 {
+
+  @Override
+  protected void serviceMainVoid() {
+		
+		
+	}
+
+  
+  
+	@Override
+	protected void DestroyCleaner() {
+		
+		
+	}	
+  
 }
